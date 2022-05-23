6358b3df9ae66fc09e59b20a05bf46e1ef9a3e7c
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2022-05-23 20:39:30 +0700

    Update

--- game/sudoku/sudokuAI.py
+++ game/sudoku/sudokuAI.py
@@ -1,14 +1,17 @@
 #solve sudoku game with AI backtracking
+from itertools import count
+import time
+
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
+    [7,0,0,4,0,0,1,2,0],
+    [6,0,0,0,7,0,0,0,0],
     [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
+    [0,0,7,0,4,0,0,6,0],
+    [0,0,1,0,5,0,0,3,0],
     [9,0,4,0,6,0,0,0,5],
     [0,7,0,3,0,0,0,1,2],
     [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [0,4,9,2,0,0,0,0,7]
 ]
 
 def print_board(bo):
@@ -56,26 +59,31 @@ def valid(bo, num, pos):
 
 
 def solve(bo):
-    find = find_empty(bo) #find empty space
-    if not find: #if no empty space
+    find = find_empty(bo) #find empty space in board complexity O(n^2)
+    if not find: #if no empty space complexity O(1)
         return True #solved
-    else: #if empty space
+    else: #if empty space complexity O(n^2)
         row, col = find #row, col
+    
+    #backtracking
+    for i in range(1, 10): #try numbers 1-9 complexity O(n)
+        if valid(bo, i, (row, col)): #check if number is valid complexity O(n^2)
+            bo[row][col] = i #if valid, set number complexity O(1)
 
-    for i in range(1, 10): #try numbers 1-9
-        if valid(bo, i, (row, col)): #check if number is valid
-            bo[row][col] = i #if valid, set number
-
-            if solve(bo): #if solved
+            if solve(bo): #if solved complexity O(n^2)
                 return True #solved
 
-            bo[row][col] = 0 #if not solved, set number to 0
+            bo[row][col] = 0 #if not solved, set number to 0 complexity O(1)
 
-    return False #not solved
-
+    return False #not solved complexity O(n^2)
 
 print_board(board)
-solve(board)
+#time start
+start = time.time()
+solve(board) #solve sudoku complexity O(n!)
+#time end
+end = time.time()
 print("___________________")
 print("Solve:")
 print_board(board)
+print("\nTime: ", end - start)
--- lab/Twitter/tweet.py
+++ lab/Twitter/tweet.py
@@ -1,8 +1,9 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import re
 from textblob import TextBlob
 
-query = "(#masker) (#jokowi) until:2022-01-01 since:2017-01-01"
+query = "(#masker) && (#jokowi) until:2022-01-01 since:2020-01-01"
 tweets = []
 limits = 100
 
@@ -10,18 +11,39 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
   if len(tweets) == limits:
     break
   else:
-    tweets.append([tweet.date, tweet.username, tweet.content])
+    tweet_properties = {}
+    tweet_properties['Date'] = tweet.date
+    tweet_properties['User'] = tweet.username
+    tweet_bersih = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",tweet.content).split())
+    tweet_properties['Tweet'] = tweet_bersih
 
+    analysis = TextBlob(tweet_bersih)
+    tweet_properties['Polarity'] = analysis.sentiment.polarity
 
-df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
-df['Polarity'] = df['Tweet'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    #check polarity
+    if analysis.sentiment.polarity > 0.0:
+      tweet_properties['Sentiment'] = 'Positive'
+    elif analysis.sentiment.polarity < 0.0:
+      tweet_properties['Sentiment'] = 'Negative'
+    else:
+      tweet_properties['Sentiment'] = 'Neutral'
 
-#check polarity
-if df['Polarity'].mean() > 0.0:
-  df['Sentiment'] = 'Positive'
-elif df['Polarity'].mean() < 0.0:
-  df['Sentiment'] = 'Negative'
-else:
-  df['Sentiment'] = 'Neutral'
-#print all
+    tweets.append(tweet_properties)
+
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'Polarity', 'Sentiment'])
 print(df)
+
+if tweet.retweetCount > 0:
+  if tweet_properties not in tweets:
+    tweets.append(tweet_properties)
+else:
+  tweets.append(tweet_properties)
+
+text_pos = [t for t in tweets if t['Sentiment'] == 'Positive']
+text_neg = [t for t in tweets if t['Sentiment'] == 'Negative']
+text_neu = [t for t in tweets if t['Sentiment'] == 'Neutral']
+
+print("Sentiment Analysis: ")
+print("Positive: ", len(text_pos), "({} %)".format(round(len(text_pos)/len(df['Tweet'])*100, 2)))
+print("Negative: ", len(text_neg), "({} %)".format(round(len(text_neg)/len(df['Tweet'])*100, 2)))
+print("Neutral: ", len(text_neu), "({} %)".format(round(len(text_neu)/len(df['Tweet'])*100, 2)))
