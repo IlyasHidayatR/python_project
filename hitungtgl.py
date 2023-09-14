170071f6a20944bee3b00e43a09aa76c98eb269b
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2023-09-14 10:40:12 +0700

    Update Program

--- hitungtgl.py
+++ hitungtgl.py
@@ -12,7 +12,33 @@ def hitungMinggu(tgl, bln, thn):
     hari = hari.isocalendar()
     return hari[1]
 
-tgl = input("Masukkan tanggal: ")
-bln = input("Masukkan bulan: ")
-thn = input("Masukkan tahun: ")
-print("Minggu ke-", hitungMinggu(tgl, bln, thn))
+def hitungTgl(minggu, tahun):
+    minggu = int(minggu)
+    tahun = int(tahun)
+    # output: (tahun, bulan, tanggal) hingga (tahun, bulan, tanggal)
+    tgl_awal = datetime.date(tahun, 1, 1)
+    tgl_akhir = datetime.date(tahun, 12, 31)
+    tgl_awal = tgl_awal.fromisocalendar(tahun, minggu, 1)
+    tgl_akhir = tgl_akhir.fromisocalendar(tahun, minggu, 7)
+    output = ('Minggu ke- ' + str(minggu) + ' tahun ' + str(tahun) + ': ' + str(tgl_awal) + ' hingga ' + str(tgl_akhir))
+    return output
+
+def main():
+    print('Menghitung minggu ke berapa suatu tanggal pada tahun tertentu')
+    print('1. Hitung minggu ke berapa')
+    print('2. Hitung tanggal dari minggu ke berapa')
+    pilih = int(input('Pilih: '))
+    if pilih == 1:
+        tgl = int(input('Tanggal: '))
+        bln = int(input('Bulan: '))
+        thn = int(input('Tahun: '))
+        print(f'Minggu ke {hitungMinggu(tgl, bln, thn)}')
+    elif pilih == 2:
+        minggu = int(input('Minggu: '))
+        tahun = int(input('Tahun: '))
+        print(f'{hitungTgl(minggu, tahun)}')
+    else:
+        print('Pilihan salah')
+
+if __name__ == '__main__':
+    main()
