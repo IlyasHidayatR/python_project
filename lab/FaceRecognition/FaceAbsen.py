0595b17b6c60fbb0921441d7b7f195164df35f8f
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2023-09-27 16:08:17 +0700

    a Little update for attendance

--- lab/FaceRecognition/FaceAbsen.py
+++ lab/FaceRecognition/FaceAbsen.py
@@ -24,6 +24,9 @@ attendance = 'lab/FaceRecognition/attendance.csv'
 
 # Main function
 def main():
+
+    Condition = True
+
     # Get the current date
     now = datetime.datetime.now()
     date = now.strftime("%Y-%m-%d")
@@ -80,44 +83,57 @@ def main():
         # Detect faces in the image
         faces = faceDetections.detectMultiScale(tampil, scaleFactor=1.3, minNeighbors=5)
 
-        # Draw a rectangle around the face
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Draw a rectangle around the 
+        if Condition == True:
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            # Recognize the face (calculate the distance between the face and the faces in the dataset)
-            id, conf = recognizer.predict(cv2.resize(tampil[y:y+h,x:x+w], (280, 280)))
+                # Recognize the face (calculate the distance between the face and the faces in the dataset)
+                id, conf = recognizer.predict(cv2.resize(tampil[y:y+h,x:x+w], (280, 280)))
 
-            # If the distance is less than 500, then the face is recognized
-            if conf > 500:
-                if id == 1:
-                    id = "Ilyas Hidayat Rusdy"
-                    # print confidence dalam persen
-                    window['-OUTPUT-'].update(id)
-                    conf = "{0}%".format(round(conf-100))
-                    print(conf)
-                    a = a + 1
-                    if a == 1:
-                        # Make date move everyday
-                        day = datetime.datetime.now()
-                        date = day.strftime("%Y-%m-%d")
-                        # Make time move every second
-                        now = datetime.datetime.now()
-                        time = now.strftime("%H:%M:%S")
-                        names.append(id)
-                        dates.append(date)
-                        times.append(time)
-                        df['Name'] = names
-                        df['Date'] = dates
-                        df['Time'] = times
-                        with open(attendance, 'a+', newline='') as csvFile:
-                            csvWriter = csv.writer(csvFile)
-                            csvWriter.writerow([id, date, time])
-                            
+                # If the distance is less than 500, then the face is recognized
+                if conf > 500:
+                    if id == 1:
+                        id = "Ilyas Hidayat Rusdy"
+                        # print confidence dalam persen
+                        window['-OUTPUT-'].update(id)
+                        conf = "{0}%".format(round(conf-100))
+                        print(conf)
+                        a = a + 1
+                        if a == 1:
+                            # Make date move everyday
+                            day = datetime.datetime.now()
+                            date = day.strftime("%Y-%m-%d")
+                            # Make time move every second
+                            now = datetime.datetime.now()
+                            time = now.strftime("%H:%M:%S")
+                            names.append(id)
+                            dates.append(date)
+                            times.append(time)
+                            if len(names) != 0:
+                                df['Name'] = names
+                                df['Date'] = dates
+                                df['Time'] = times
+                                with open(attendance, 'a+', newline='') as csvFile:
+                                    csvWriter = csv.writer(csvFile)
+                                    csvWriter.writerow([names[-1], dates[-1], times[-1]])
+                            elif len(names) == 0:
+                                names.append('Name')
+                                dates.append('Date')
+                                times.append('Time')
+                                df['Name'] = names
+                                df['Date'] = dates
+                                df['Time'] = times
+                                with open(attendance, 'a+', newline='') as csvFile:
+                                    csvWriter = csv.writer(csvFile)
+                                    csvWriter.writerow(['Name', 'Date', 'Time'])
+                    else:
+                        id = "Unknown"
                 else:
                     id = "Unknown"
-            else:
-                id = "Unknown"
-            cv2.putText(frame, str(id), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(frame, str(id), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, "Face Recognition Stopped", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         # Display the resulting frame
         imgbytes = cv2.imencode('.png', frame)[1].tobytes()
@@ -139,44 +155,43 @@ def main():
 
         # If the 'Start' button is clicked, then start the program
         if event == 'Start':
-            # If the 'Start' button is clicked for the first time, then start the program
-            if b == 0:
-                a = 0
-                b = 1
+            # If the 'Start' button is clicked for the first time, then start the camera
+            if a == 0:
+                a = 1
+                b = 0
                 c = 0
                 d = 0
                 e = 0
+                Condition = True
+                # Initialize the list
                 names = []
                 dates = []
                 times = []
+                # Initialize the dataframe
                 df = pd.DataFrame()
+                # Initialize the csv file
                 with open(attendance, 'a+', newline='') as csvFile:
                     csvWriter = csv.writer(csvFile)
                     csvWriter.writerow(['Name', 'Date', 'Time'])
-
+                    
             # If the 'Start' button is clicked for the second time, then continue the program
             if b == 1:
                 b = 0
                 c = 0
                 d = 0
                 e = 1
+                Condition = True
 
         # If the 'Stop' button is clicked, then stop the program
         if event == 'Stop':
-            # If the 'Stop' button is clicked for the first time, then stop the program
-            if c == 0:
+            # If the 'Stop' button is clicked for the first time, then pause the program
+            if b == 0:
                 a = 0
-                b = 0
-                c = 1
+                b = 1
+                c = 0
                 d = 0
                 e = 0
-                names = []
-                dates = []
-                times = []
-                df = pd.DataFrame()
-                with open(attendance, 'a+', newline='') as csvFile:
-                    csvWriter = csv.writer(csvFile)
-                    csvWriter.writerow(['Name', 'Date', 'Time'])
+                Condition = False
 
         # If the 'Show Attendance' button is clicked, then show the attendance
         if event == 'Show Attendance':
@@ -198,13 +213,16 @@ def main():
                 c = 0
                 d = 0
                 e = 1
+                Condition = False
+                # Initialize the list
                 names = []
                 dates = []
                 times = []
+                # Initialize the dataframe
                 df = pd.DataFrame()
+                # Initialize the csv file
                 with open(attendance, 'w', newline='') as csvFile:
                     csvWriter = csv.writer(csvFile)
-                    csvWriter.writerow(['Name', 'Date', 'Time'])
 
         # If the 'Record Face' button is clicked, then record the face
         if event == 'Record Face':
